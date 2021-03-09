4a46baf1d4c71750bc5f2ec7ec0814d484640b74
Jaydawg8888 <36142372+Jaydawg8888@users.noreply.github.com>
2021-03-09 21:09:43 +0000

    Add project files.

---
+++ NorseCore/CoreGlobal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NorseCore
+{
+    public static class CoreGlobal
+    {
+        // I really really dislike globals, but its easier to call CoreGlobal so the dependencies all line up :/
+        public const string pluginGUID = "org.bepinex.plugins.norse_core";
+        public const string pluginName = "Norse Core";
+        public const string pluginVersion = "1.0.0.0";
+    }
+}
---
+++ NorseCore/NorseCore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using NorseCore;
+
+namespace NorseCore
+{
+    [BepInPlugin(CoreGlobal.pluginGUID, CoreGlobal.pluginName, CoreGlobal.pluginVersion)]
+    public class NorseCore : BaseUnityPlugin
+    {
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            Logger.LogInfo("Norse Core: Loaded");
+
+            Harmony harmony = new Harmony("mod.norse_core");
+            harmony.PatchAll();
+        }
+    }
+}
